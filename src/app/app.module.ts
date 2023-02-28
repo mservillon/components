34b391fb7f3fe7e7e82c977d443f33a911ebeb84
mservillon <mackservillon@gmail.com>
2023-02-28 11:52:20 -0600

    Generated an albums component!!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,11 +10,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MackAlbumListComponent } from './mack-album-list/mack-album-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PizzaToppingsComponent
+    PizzaToppingsComponent,
+    MackAlbumListComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/mack-album-list/mack-album-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mack-album-list',
+  templateUrl: './mack-album-list.component.html',
+  styleUrls: ['./mack-album-list.component.css']
+})
+export class MackAlbumListComponent {
+
+}
